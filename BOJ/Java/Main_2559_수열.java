5f29aa30c25d7ba22d87a787449e9fae33bcf736
jjoyra <bunny7531@naver.com>
2023-06-12 23:09:15 +0900

    [BOJ] 2559 수열

---
+++ BOJ/Java/Main_2559_수열.java
@@ -0,0 +1,40 @@
+package algorithm.BOJ.Java;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_2559_수열 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int answer = Integer.MIN_VALUE;
+        int window = 0;
+
+        for(int i = 0; i < K; i++) {
+            window += arr[i];
+        }
+
+        answer = Math.max(answer, window);
+
+        for(int i = K; i < N; i++) {
+            window -= arr[i - K];
+            window += arr[i];
+            answer = Math.max(answer, window);
+        }
+
+        System.out.println(answer);
+
+    }
+}
