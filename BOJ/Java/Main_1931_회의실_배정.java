009c531e07e3db5ae70025dd96d8c6cea4a46ecc
jjoyra <bunny7531@naver.com>
2023-06-19 16:05:09 +0900

    [BOJ] 1931 회의실 배정

---
+++ BOJ/Java/Main_1931_회의실_배정.java
@@ -0,0 +1,44 @@
+package algorithm.BOJ.Java;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main_1931_회의실_배정 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+
+        int N = Integer.parseInt(br.readLine());
+        int answer = 0;
+        int lastTime = -1;
+
+        int[][] arr = new int[N][2];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+
+        }
+
+        // 끝나는 시간, 끝나는 시간이 같을 경우 시작 시간 기준으로 정렬
+        Arrays.sort(arr, (a, b) -> {
+            if(a[1] != b[1]) return a[1] - b[1];
+            else return a[0] - b[0];
+        });
+
+        for(int i = 0; i < N; i++) {
+            if(lastTime <= arr[i][0]) {
+                lastTime = arr[i][1];
+                answer++;
+            }
+        }
+
+        System.out.println(answer);
+
+    }
+}
