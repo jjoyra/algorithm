e05249bf2c9d80399ac69bad63747b2d76d7de2d
jjoyra <bunny7531@naver.com>
2024-01-04 20:37:24 +0900

    [BOJ] 2012 등수 매기기

---
+++ BOJ/Java/Main_2012_등수_매기기.java
@@ -0,0 +1,28 @@
+package algorithm.BOJ.Java;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main_2012_등수_매기기 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        for(int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr);
+
+        long answer = 0;
+
+        for(int i = 0; i < N; i++) {
+            answer += Math.abs(arr[i] - (i + 1));
+        }
+
+        System.out.println(answer);
+    }
+}
